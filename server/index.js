730372b9cbe4cfc666342114c8317a2b4a6d7f03
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-22 19:05:20 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ UserModel.find({})
 
 app.get('/getUser/:id',(req,res)=>{
 const id=req.params.id;
-    UserModel.findById({_id:id})
+UserModel.findById({_id:id})
     .then(users=>res.json(users))
     .catch(err=>res.json(err))
 })
